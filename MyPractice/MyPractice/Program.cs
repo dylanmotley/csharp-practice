dc3808ed1bab4a3c1e58572c48514b04c0dcac62
Dyaln Motley <motley.dylan60@gmail.com>
2021-09-08 14:06:13 -0400

    First Commit

---
+++ MyPractice/MyPractice/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace MyPractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            String choice = "y";
+            while (choice.Equals("y"))
+            {
+
+                Console.WriteLine("Enter Number: ");
+                int num1 = Convert.ToInt32(Console.ReadLine());
+
+                int total = num1 * num1;
+
+                Console.WriteLine(total);
+
+                Console.WriteLine("Continue? (y/n)");
+                choice = Console.ReadLine();
+            }
+
+
+        }
+    }
+}
